90f7f62f68f81303eec3597a1fd1aa7a4ab2b873
astefanski1 <aleksander.stefanski@outlook.com>
2019-10-17 18:04:45 +0200

    First commit

---
+++ src/main/java/com/ssa/SpotifyMusicSearchApplication/SpotifyMusicSearchApplication.java
@@ -0,0 +1,13 @@
+package com.ssa.SpotifyMusicSearchApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyMusicSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyMusicSearchApplication.class, args);
+	}
+
+}
